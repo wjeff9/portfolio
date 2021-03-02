539c020ddf27891b6249b66353ce7df7645c75d6
Jeffrey <youngjeff21w@gmail.com>
2021-03-02 03:05:39 -0800

    fix typos

--- src/ProjEscape.js
+++ src/ProjEscape.js
@@ -28,7 +28,7 @@ function ProjEscape(props) {
             <br />
             <h2 className="title2">Team</h2>
             <p className="article">
-                I'm working on this project with a few college friends. Andy Cahill and John Rosen are the developers, Darren Ma is the project manager, and I'm the UX designer. We are being mentored by Chris Coward, the researcher behind the project, and Lindsay Morsel, a game desinger.
+                I'm working on this project with a few college friends. Andy Cahill and John Rosen are the developers, Darren Ma is the project manager, and I'm the UX designer. Chris Coward, the researcher behind the project, and Lindsay Morsel, a game desinger, are our mentors.
             </p>
             <br />
             <h1 className="title1">Usability Testing</h1>
@@ -44,25 +44,25 @@ function ProjEscape(props) {
             </p>
             <br />
             <p className="article">
-                The original design was not super intuitive but in this context, it is actually fine. In a real escape room, the last thing your captor would do is give you clear instructions on how to escape. Not being 100% clear makes the game more challenging, but more rewarding to solve.
+                The original design was not super intuitive but in this context, it is actually not an issue. In a real escape room, the last thing your captor would do is give you clear instructions on how to escape. Not being absolutely clear makes the game more challenging and more rewarding to solve.
             </p>
             <br />
             <p className="article">
-                The main problem with the first puzzle was immersion. The first puzzle was just thrown together with some text in a questionablee font pairing on a default white background, which hurted to look at.
+                The main problem with the first puzzle was immersion. The first puzzle was just thrown together with some text in a questionable font pairing on a default white background, which hurt to look at.
             </p>
             <br />
             <h2 className="title2">Puzzle 1: After</h2>
             <img src={Puzzle1} alt="Mark Novo website template" className="proj-photo" />
             <p className="article">
-                This is my Figma redesign of the first puzzle. In real life, escape rooms use unsuspecting objects such as paintings and clocks on a wall to give players clues. To simulate the feelibg of being in a real escape room, I decided use a skeuomorphic (neumorphic) theme. I played with gradients and drop-shadows to give the environement a 3D effect. I'm aware that the modern feel of this theme does not give off a creepy vibe like most escape rooms, but our mentor felt that making the game intense/scary would take away from its educational aspect (which is debatable).
+                This is my Figma redesign of the first puzzle. In real life, escape rooms use unsuspecting objects such as paintings and clocks on a wall to give players clues. To simulate the feeling of being in a real escape room, I decided use a skeuomorphic (neumorphic) theme. I played with gradients and drop-shadows to give the environement a 3D effect. I'm aware that the modern feel of this theme does not give off a creepy vibe like most escape rooms, but our mentor felt that making the game intense/scary would take away from its educational aspect (which is debatable).
             </p>
             <br />
             <p className="article">
-                I transformed the cards with gray stars into post-it notes to emphasize that they were meant to be moved around. I might have been inspired by Miro boards after using them in classes all quarter. Next, I translated the gold stars into tacks and recolored the background tan to imitate a pin board. These represent where to place the post-it notes. The hardest item to redesign were the purple circles. Initially, I wanted to use highlighter marks, but in real life, they cannot transfer from the pin board onto the post-it notes. I settled on lasers, which shine over the pin board and the post-it notes so dragging the post-it notes around would interact realistically with the environment.
+                I transformed the cards with gray stars into post-it notes to emphasize that they were meant to be moved around. I might have been inspired by Miro after using it all quarter (see my usability testing notes). Next, I translated the gold stars into tacks and recolored the background tan to imitate a pin board. These represent where to place the post-it notes. The hardest item to redesign were the purple circles. Initially, I wanted to use highlighter marks, but in real life, they cannot transfer from the pin board onto the post-it notes. I settled on lasers, which shine over the pin board and the post-it notes so dragging the post-it notes around would interact realistically with the environment.
             </p>
             <br />
             <p className="article">
-                By making the puzzle more immersive, I also unintentionally made it easier to solve, so to compensate, I resized the tacks and lasers to a similar size. With previous version, players would often try to match the gray stars with the purple circles, so I tried to bring back this confusion with the new design.
+                By making the puzzle more immersive, I also unintentionally made it easier to solve, so to compensate, I altered the tacks and lasers to a similar size. With the previous version, players would often try to match the gray stars with the purple circles, so I tried to bring back this confusion with the similarly sized items.
             </p>
             <br />
             <p className="article">
