45057494e75b0752b33eaa68a8720ce4f1749b3f
Jeffrey <youngjeff21w@gmail.com>
2021-03-11 03:40:58 -0800

    update intro

--- src/About.js
+++ src/About.js
@@ -10,12 +10,17 @@ function About(props) {
             <div className="about-col2">
                 <h2 className="intro-head">
                     Hello, <br />
-                    I’m a UX designer based in Palo Alto, CA.
+                    I’m Jeffrey, <br />
+                    a UX Designer.
                 </h2>
                 <br /><br />
                 <p className="intro">
-                    I am currently a senior studying informatics with a focus in HCI at the University of Washington. I chose this career path because I enjoy doing creative work and interacting with others. I care about designing products that are original, impactful, and of course, usable. 
+                    I study informatics with a focus in human-computer interaction at the University of Washington. I chose this career because I enjoy sketching, being creative, and interacting with others. I care about designing interfaces that are intuitive, accessible, and beautiful. 
                 </p>
+                <br />
+                {/* <p className="intro">
+                    Compared to my peers, I have a strong technical background. Half of my classes are design related and the other half are coding.
+                </p> */}
                 {/* <p className="intro">
                 <br />
                    My methodology is fairly standard and can be summarized in 3 steps. First, I <b>discover</b> the situation by doing research. Next, I <b>define</b> the problems that need to be addressed. With clear goals written out, I <b>design</b> a new solution—but it doesn't end there. The new solution becomes a prototype that needs to go through usability testing, which takes me back to the <b>discover</b> phase. The cycle repeats until the outcome is satisfactory to me and approved by my users.
